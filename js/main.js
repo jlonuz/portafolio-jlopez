f57a21620558077654d766dfef856f143f783b6e
jlonuz <lonuz.j@gmail.com>
2024-02-22 19:23:42 -0300

    add files

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// $('.grid').isotope({
+//     // options
+//     itemSelector: '.grid-item',
+//     masonry: {
+//         gutter: '.gutter'
+//     }
+// });
+
+
+$('.grid').isotope({
+    itemSelector: '.grid-item',
+    percentPosition: true,
+    masonry: {
+        gutter: '.gutter'
+    }
+});
